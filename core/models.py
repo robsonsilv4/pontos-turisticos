87f8dd6bc16ac17bd633e3bbb3021f0bad4af9a8
robsonsilv4 <robsonsilv410@gmail.com>
2018-12-11 16:18:31 -0300

    Modelando app de comentários e avaliações

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from atracoes.models import Atracao
+from comentarios.models import Comentario
+from avaliacoes.models import Avaliacao
 
 
 class PontoTuristico(models.Model):
@@ -7,6 +9,8 @@ class PontoTuristico(models.Model):
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
     atracoes = models.ManyToManyField(Atracao)
+    comentarios = models.ManyToManyField(Comentario)
+    avaliacoes = models.ManyToManyField(Avaliacao)
 
     def __str__(self):
         return self.nome
