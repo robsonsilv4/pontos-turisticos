bccb75269b027c0c6524f33c72236a9c746cf140
robsonsilv4 <robsonsilv410@gmail.com>
2018-12-22 07:08:15 -0300

    Implementando actions personalizadas

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
@@ -31,4 +32,12 @@ class PontoTuristicoViewSet(ModelViewSet):
         pass
 
     def partial_update(self, request, *args, **kwargs):
+        pass
+
+    @action(methods=['get', 'post'], detail=True)
+    def denunciar(self, request, pk=None):
+        pass
+
+    @action(methods=['get'], detail=False)
+    def teste(self, request):
         pass
